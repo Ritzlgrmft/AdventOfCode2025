f0099f1557e8149850d2328ebf9285dabfbb5b05
Markus Wagner <markus.wagner02@web.de>
2025-12-07 12:41:04 +0100

    add day 3

---
+++ Day3a/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AdventOfCode2025.Day3a;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var banks = File.ReadLines(inputFile);
+        var sum = 0L;
+        foreach (var bank in banks)
+        {
+            var joltage1 = bank.Substring(0, bank.Length - 1).Max();
+            var joltage2 = bank.Substring(bank.IndexOf(joltage1) + 1).Max();
+            var joltage = (joltage1 - '0') * 10 + (joltage2 - '0');
+            sum += joltage;
+        }
+        return sum;
+    }
+}
---
+++ Day3b/Worker.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+
+namespace AdventOfCode2025.Day3b;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        const int numberOfBatteries = 12;
+
+        var banks = File.ReadLines(inputFile);
+        var sum = 0L;
+        foreach (var bank in banks)
+        {
+            var joltage = 0L;
+            var remainingBank = bank;
+            for (var battery = 1; battery <= numberOfBatteries; battery++)
+            {
+                var biggestValue = remainingBank.Substring(0, remainingBank.Length - numberOfBatteries + battery).Max();
+                joltage = joltage * 10 + (biggestValue - '0');
+                remainingBank = remainingBank.Substring(remainingBank.IndexOf(biggestValue) + 1);
+            }
+
+            sum += joltage;
+        }
+        return sum;
+    }
+}
