8564b486a254c223964ecdb922485fbce8d78e83
Markus Wagner <markus.wagner02@web.de>
2025-12-07 13:51:24 +0100

    add day 5

---
+++ Day5a/Worker.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode2025.Day5a;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var freshIngredients = new List<(long min, long max)>();
+        var availableIngredients = new List<long>();
+        var readFreshIngredients = true;
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                readFreshIngredients = false;
+            }
+            else if (readFreshIngredients)
+            {
+                var parts = line.Split('-');
+                freshIngredients.Add((long.Parse(parts[0]), long.Parse(parts[1])));
+            }
+            else
+            {
+                availableIngredients.Add(long.Parse(line));
+            }
+        }
+
+        var sum = 0L;
+        foreach (var ingredient in availableIngredients)
+        {
+            var isFresh = false;
+            foreach (var freshIngredient in freshIngredients)
+            {
+                if (ingredient >= freshIngredient.min && ingredient <= freshIngredient.max)
+                {
+                    isFresh = true;
+                    break;
+                }
+            }
+            if (isFresh)
+            {
+                sum++;
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ Day5b/Worker.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode2025.Day5b;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var freshIngredients = new List<(long min, long max)>();
+        var readFreshIngredients = true;
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                readFreshIngredients = false;
+            }
+            else if (readFreshIngredients)
+            {
+                var parts = line.Split('-');
+                freshIngredients.Add((long.Parse(parts[0]), long.Parse(parts[1])));
+            }
+        }
+
+        bool ingredientsConsolidated;
+        do
+        {
+            ingredientsConsolidated = false;
+            for (var i = 0; i < freshIngredients.Count && !ingredientsConsolidated; i++)
+            {
+                for (var j = i + 1; j < freshIngredients.Count && !ingredientsConsolidated; j++)
+                {
+                    var first = freshIngredients[i];
+                    var second = freshIngredients[j];
+                    if (first.max >= second.min && second.max >= first.min)
+                    {
+                        var newIngredient = (min: Math.Min(first.min, second.min), max: Math.Max(first.max, second.max));
+                        freshIngredients.RemoveAt(j);
+                        freshIngredients.RemoveAt(i);
+                        freshIngredients.Add(newIngredient);
+                        ingredientsConsolidated = true;
+                    }
+                }
+            }
+
+        } while (ingredientsConsolidated);
+
+        var sum = freshIngredients.Sum(ingredient => ingredient.max - ingredient.min + 1);
+        return sum;
+    }
+}
