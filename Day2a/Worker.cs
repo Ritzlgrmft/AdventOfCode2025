2f7dabb22ad67c2008912336c3c25817cd781f47
Markus Wagner <markus.wagner02@web.de>
2025-12-07 08:51:11 +0100

    add day 2

---
+++ Day2a/Worker.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2025.Day2a;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var rangesLeft = new List<long>();
+        var rangesRight = new List<long>();
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            foreach (var part in line.Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                var ranges = part.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                rangesLeft.Add(long.Parse(ranges[0]));
+                rangesRight.Add(long.Parse(ranges[1]));
+            }
+        }
+
+        var sum = 0L;
+        for (var i = 0; i < rangesLeft.Count; i++)
+        {
+            for (var productId = rangesLeft[i]; productId <= rangesRight[i]; productId++)
+            {
+                var formattedId = productId.ToString();
+                if (formattedId.Length % 2 == 0)
+                {
+                    var halfLength = formattedId.Length / 2;
+                    var leftPart = formattedId[..halfLength];
+                    var rightPart = formattedId[halfLength..];
+                    if (leftPart == rightPart)
+                    {
+                        sum += productId;
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+}
---
+++ Day2b/Worker.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode2025.Day2b;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var rangesLeft = new List<long>();
+        var rangesRight = new List<long>();
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            foreach (var part in line.Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                var ranges = part.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                rangesLeft.Add(long.Parse(ranges[0]));
+                rangesRight.Add(long.Parse(ranges[1]));
+            }
+        }
+
+        var sum = 0L;
+        for (var i = 0; i < rangesLeft.Count; i++)
+        {
+            for (var productId = rangesLeft[i]; productId <= rangesRight[i]; productId++)
+            {
+                var formattedId = productId.ToString();
+                var isValid = true;
+                for (var partLength = 1; partLength <= formattedId.Length / 2 && isValid; partLength++)
+                {
+                    var part = formattedId[..partLength];
+                    if (string.Concat(Enumerable.Repeat(part, formattedId.Length / partLength)) == formattedId)
+                    {
+                        isValid = false;
+                    }
+                }
+                if (!isValid)
+                {
+                    sum += productId;
+                }
+            }
+        }
+        return sum;
+    }
+}
