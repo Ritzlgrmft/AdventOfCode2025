40d24253909b9c3802a19a94be723beb4f44d7e9
Markus Wagner <markus.wagner02@web.de>
2025-12-07 12:10:32 +0100

    add day 7

---
+++ Day7a/Worker.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode2025.Day7a;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var lines = File.ReadLines(inputFile);
+        var beams = new List<int>() { lines.First().IndexOf('S') };
+
+        var splitted = 0L;
+        foreach (var line in lines.Skip(1))
+        {
+            var newBeams = new List<int>();
+            foreach (var beam in beams)
+            {
+                if (line[beam] == '.')
+                {
+                    newBeams.Add(beam);
+                }
+                else if (line[beam] == '^')
+                {
+                    newBeams.Add(beam - 1);
+                    newBeams.Add(beam + 1);
+                    splitted++;
+                }
+            }
+
+            beams = newBeams.Distinct().ToList();
+        }
+
+        return splitted;
+    }
+}
---
+++ Day7b/Worker.cs
@@ -0,0 +1,54 @@
+﻿
+namespace AdventOfCode2025.Day7b;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var lines = File.ReadLines(inputFile);
+        var beams = new List<Beam>() { new() { Pos = lines.First().IndexOf('S'), Count = 1 } };
+
+        var splitted = 0L;
+        foreach (var line in lines.Skip(1))
+        {
+            var newBeams = new List<Beam>();
+            foreach (var beam in beams)
+            {
+                if (line[beam.Pos] == '.')
+                {
+                    AddBeam(newBeams, beam.Pos, beam.Count);
+                }
+                else if (line[beam.Pos] == '^')
+                {
+                    AddBeam(newBeams, beam.Pos - 1, beam.Count);
+                    AddBeam(newBeams, beam.Pos + 1, beam.Count);
+                    splitted++;
+                }
+            }
+
+            beams = newBeams;
+        }
+
+        return beams.Sum(b => b.Count);
+    }
+
+    private void AddBeam(List<Beam> newBeams, int pos, long count)
+    {
+        var beam = newBeams.FirstOrDefault(b => b.Pos == pos);
+        if (beam != null)
+        {
+            beam.Count += count;
+        }
+        else
+        {
+            newBeams.Add(new Beam { Pos = pos, Count = count });
+        }
+    }
+
+    private class Beam
+    {
+        public int Pos { get; set; }
+        public long Count { get; set; }
+        public override string ToString() => $"Pos: {Pos}, Count: {Count}";
+    }
+}
