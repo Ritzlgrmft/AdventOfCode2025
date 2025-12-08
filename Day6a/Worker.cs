1087e04367f3fafbc1e711bae6395c77f6bf4a7a
Markus Wagner <markus.wagner02@web.de>
2025-12-08 07:18:54 +0100

    add day 6

---
+++ Day6a/Worker.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2025.Day6a;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var values = new List<long[]>();
+        string[] operators = [];
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                values.Add(parts.Select(long.Parse).ToArray());
+            }
+            catch
+            {
+                operators = parts;
+            }
+        }
+
+        var sum = 0L;
+        for (var i = 0; i < operators.Length; i++)
+        {
+            var op = operators[i];
+            var result = op == "+" ? 0L : 1L;
+            for (var j = 0; j < values.Count; j++)
+            {
+                result = op == "+" ? result + values[j][i] : result * values[j][i];
+            }
+            sum += result;
+        }
+
+        return sum;
+    }
+}
---
+++ Day6b/Worker.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode2025.Day6b;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var data = File.ReadLines(inputFile).ToArray();
+        var operators = data.Last();
+
+        var sum = 0L;
+        var op = 'x';
+        var result = 0L;
+        for (var col = 0; col < operators.Length; col++)
+        {
+            if (operators[col] != ' ')
+            {
+                op = operators[col];
+                sum += result;
+                result = op == '+' ? 0L : 1L;
+            }
+
+            var value = 0L;
+            for (var row = 0; row < data.Length - 1; row++)
+            {
+                if (data[row][col] != ' ')
+                {
+                    value = value * 10 + (data[row][col] - '0');
+                }
+            }
+
+            if (value > 0)
+            {
+                result = op == '+' ? result + value : result * value;
+            }
+        }
+        sum += result;
+
+        return sum;
+    }
+}
