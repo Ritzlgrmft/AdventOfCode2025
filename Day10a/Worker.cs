0d984c261cf121f1f98405de7b4ee6f2d4ab80b7
Markus Wagner <markus.wagner02@web.de>
2025-12-10 10:54:25 +0100

    add day 10

---
+++ Day10a/Worker.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode2025.Day10a;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var targets = new List<string>();
+        var buttons = new List<int[][]>();
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var parts = line.Split(" (){}[]".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            targets.Add(parts[0]);
+            buttons.Add(parts
+                .Skip(1)
+                .SkipLast(1)
+                .Select(p => p.Split(',').Select(int.Parse).ToArray())
+                .ToArray());
+        }
+
+        var sum = 0L;
+        for (var machine = 0; machine < targets.Count; machine++)
+        {
+            sum += PressButtons(targets[machine], buttons[machine]);
+        }
+        return sum;
+    }
+
+    private long PressButtons(string target, int[][] buttonSet)
+    {
+        var presses = 0L;
+        var states = new List<string>() { target.Replace('#', '.') };
+        while (true)
+        {
+            presses++;
+            var newStates = new List<string>();
+            foreach (var state in states)
+            {
+                foreach (var button in buttonSet)
+                {
+                    var newState = pressButton(state, button);
+                    if (target == newState)
+                    {
+                        return presses;
+                    }
+                    newStates.Add(newState);
+                }
+            }
+            states = newStates;
+        }
+    }
+
+    private string pressButton(string state, int[] button)
+    {
+        var result = state.ToCharArray();
+        foreach (var index in button)
+        {
+            result[index] = result[index] == '#' ? '.' : '#';
+        }
+        return new string(result);
+    }
+}
---
+++ Day10b/Worker.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Z3;
+
+namespace AdventOfCode2025.Day10b;
+
+public class Worker : IWorker
+{
+    public long DoWork(string inputFile)
+    {
+        var buttons = new List<int[][]>();
+        var joltages = new List<int[]>();
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            var parts = line.Split(" (){}[]".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            buttons.Add(parts
+                .Skip(1)
+                .SkipLast(1)
+                .Select(p => p.Split(',').Select(int.Parse).ToArray())
+                .ToArray());
+            joltages.Add(parts
+                .Last()
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray());
+        }
+
+        var sum = 0L;
+        for (var machine = 0; machine < joltages.Count; machine++)
+        {
+            var presses = PressButtons(joltages[machine], buttons[machine]);
+            sum += presses;
+        }
+        return sum;
+    }
+
+    private long PressButtons(int[] joltages, int[][] buttonSet)
+    {
+        using var ctx = new Context();
+        using var opt = ctx.MkOptimize();
+
+        // define variables
+        var presses = Enumerable.Range(0, buttonSet.Length)
+            .Select(i => ctx.MkIntConst($"x{i}"))
+            .ToArray();
+
+        // initialize variables
+        foreach (var press in presses)
+            opt.Add(ctx.MkGe(press, ctx.MkInt(0)));
+
+        // add linear equations
+        for (int i = 0; i < joltages.Length; i++)
+        {
+            var affecting = presses.Where((_, j) => buttonSet[j].Contains(i)).ToArray();
+            if (affecting.Length > 0)
+            {
+                opt.Add(ctx.MkEq(ctx.MkAdd(affecting), ctx.MkInt(joltages[i])));
+            }
+        }
+
+        // evaluate equations
+        opt.MkMinimize(ctx.MkAdd(presses));
+        opt.Check();
+        return presses.Sum(p => ((IntNum)opt.Model.Evaluate(p, true)).Int64);
+    }
+}
